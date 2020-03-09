68a1a5b02fd01d30778165a82bf266cb2dabb6bb
yeeun <yenn2322@gmail.com>
2020-03-09 22:51:55 +0900

    #4.2 API Verbs part One

--- src/api.js
+++ src/api.js
@@ -8,6 +8,14 @@ const api = axios.create({
   }
 });
 
-export default axios;
+export const moviesApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular")
+};
 
-api.get("tv/popular");
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today")
+};
