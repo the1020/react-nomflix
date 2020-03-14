68619381231c14244d5985713243f7e870867708
yeeun <yenn2322@gmail.com>
2020-03-14 22:04:15 +0900

    #5.3 TV Container

--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TVPresenter from "./TVPresenter";
+import { tvApi } from "api";
 
 export default class extends React.Component {
   state = {
@@ -10,8 +11,29 @@ export default class extends React.Component {
     loading: true
   };
 
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: topRated }
+      } = await tvApi.topRated();
+      const {
+        data: { results: popluar }
+      } = await tvApi.popluar();
+      const {
+        data: { results: airingToday }
+      } = await tvApi.airingToday();
+
+      this.setState({ topRated, popluar, airingToday });
+    } catch {
+      this.setState({ error: "Error Message" });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
   render() {
     const { topRated, popluar, airingToday, error, loading } = this.state;
+    console.log(topRated, popluar, airingToday, error, loading);
     return (
       <TVPresenter
         topRated={topRated}
